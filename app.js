cb04fe06f5af285e374d2fd58ebfb5a57f73f1b0
Jaejaelin <109079924+Jaejaelin@users.noreply.github.com>
2023-10-27 22:30:40 +0900

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+const nav_icon = document.querySelector('.nav_icon');
+const nav_menu = document.querySelector('.nav_menu');
+
+nav_icon.addEventListener('click', ()=>{
+    nav_menu.classList.toggle('active');
+});
